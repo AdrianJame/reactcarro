a00f79eeeeb3d809ae8cf59a84c27a55e5f96b4b
AdrianJame <adrianjamesbadwolf@gmail.com>
2023-08-29 15:28:05 -0300

    cabe pronto

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,4 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Menu(){
 
+     return(
+        <section className='menu'>
+            <div className='menu-logonome'>
+              <img src='/assets/images/logo.svg'/>
+              <h1>Elite<span>Wheels</span></h1>
+            </div>
+
+            <div className='menu-Links'>
+              <Link><img src='/assets/images/'/> Home</Link>
+              <Link><img src='/assets/images/'/> Clientes</Link>
+              <Link><img src='/assets/images/'/> Veiculos</Link>
+              <Link><img src='/assets/images/'/> Locação</Link>
+            </div>
+        </section>
+
+     )
 }
--- src/pages/cadastrocliente/index.js
+++ src/pages/cadastrocliente/index.js
@@ -5,22 +5,9 @@ import { Link } from 'react-router-dom';
 function Client() {
   return (
     <div className="CadastroClient">
-          <section className='menu'>
-            <div className='menu-logonome'>
-              <img src='/assets/images/logo.svg'/>
-              <h1>Elite<span>Wheels</span></h1>
-            </div>
-
-            <div className='menu-Links'>
-              <Link><img src='/assets/images/'/> Home</Link>
-              <Link><img src='/assets/images/'/> Clientes</Link>
-              <Link><img src='/assets/images/'/> Veiculos</Link>
-              <Link><img src='/assets/images/'/> Locação</Link>
-            </div>
-          </section>
-
+      <Menu/>
       <section className='area-administrativa'>
-        <div>
+        <div className='area-administrativa-cabe'>
           <p>Olá, que bom que você voltou!</p>
 
           <section> A </section>
